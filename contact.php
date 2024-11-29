106dabaf280f6cf627ee27d3cdb1c05324a7e04b
Sudarmika <yansudar123@gmail.com>
2024-11-29 18:34:27 +0800

    mengubah konten lokasi pada kontak dan menambahkan informasi email setiap halaman

--- contact.php
+++ contact.php
@@ -48,8 +48,8 @@ https://templatemo.com/tm-591-villa-agency
       <div class="row">
         <div class="col-lg-8 col-md-8">
           <ul class="info">
-            <li><i class="fa fa-envelope"></i> info@company.com</li>
-            <li><i class="fa fa-map"></i> Sunny Isles Beach, FL 33160</li>
+            <li><i class="fa fa-envelope"></i> skibidi12@gmail.com</li>
+            <li><i class="fa fa-map"></i> Jl. Raya Seminyak 49-53</li>
           </ul>
         </div>
         <div class="col-lg-4 col-md-4">
@@ -167,7 +167,7 @@ https://templatemo.com/tm-591-villa-agency
         </div>
         <div class="col-lg-12">
           <div id="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth" width="100%" height="500px" frameborder="0" style="border:0; border-radius: 10px; box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);" allowfullscreen=""></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.002067044599!2d115.16293572280897!3d-8.691351769003969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2472054a3e449%3A0x7abe48edc4284b38!2sJl.%20Raya%20Seminyak%2049-53%2C%20Seminyak%2C%20Kec.%20Kuta%2C%20Kabupaten%20Badung%2C%20Bali%2080361!5e0!3m2!1sid!2sid!4v1732875808708!5m2!1sid!2sid" width="100%" height="500px" frameborder="0" style="border:0; border-radius: 10px; box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);" allowfullscreen=""></iframe>
           </div>
         </div>
       </div>
